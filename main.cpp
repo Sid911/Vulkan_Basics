acfc46835349a6ee429e96c03b4509af35b05755
Siddharth Sinha <siddharthsinha9752@gmail.com>
2024-07-17 21:04:18 +0530

    Loading obj model

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@
 #include <optional>
 #include <print>
 #include <set>
+#include <unordered_map>
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include "tiny_obj_loader.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 #include <stdexcept>
@@ -59,6 +64,10 @@ struct Vertex {
     return bindingDescription;
   }
 
+  bool operator==(const Vertex &other) const {
+    return pos == other.pos && color == other.color && texCoord == other.texCoord;
+  }
+
   static auto getAttributeDescriptions() {
     std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
     attributeDescriptions[0].binding  = 0;
@@ -79,26 +88,22 @@ struct Vertex {
   }
 };
 
-// const std::vector<Vertex> vertices = {
-//     {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
-//     {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
-//     {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
-//     {{-0.5f, 0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f}}
-// };
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
-const std::vector<Vertex> vertices = { { { -0.5f, -0.5f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f } },
-  { { 0.5f, -0.5f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f } },
-  { { 0.5f, 0.5f, 0.0f }, { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f } },
-  { { -0.5f, 0.5f, 0.0f }, { 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f } },
+namespace std {
+template<>
+struct hash<Vertex> {
+  size_t operator()(Vertex const &vertex) const {
+    return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1)
+           ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
+  }
+};
+}  // namespace std
 
-  { { -0.5f, -0.5f, -0.5f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f } },
-  { { 0.5f, -0.5f, -0.5f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f } },
-  { { 0.5f, 0.5f, -0.5f }, { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f } },
-  { { -0.5f, 0.5f, -0.5f }, { 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f } } };
+std::vector<Vertex> vertices;
+std::vector<uint32_t> indices;
 
-// const std::vector<uint16_t> indices = { 0, 1, 2, 2, 3, 0 };
-
-const std::vector<uint16_t> indices = { 0, 1, 2, 2, 3, 0, 4, 5, 6, 6, 7, 4 };
 
 class BasicsApp {
 public:
@@ -115,6 +120,9 @@ private:
   bool framebufferResized        = false;
   const uint32_t WIDTH           = 800;
   const uint32_t HEIGHT          = 600;
+
+  const std::string MODEL_PATH   = "models/viking_room.obj";
+  const std::string TEXTURE_PATH = "textures/viking_room.png";
 
   GLFWwindow *window;
   VkSurfaceKHR surface{};
@@ -148,6 +156,7 @@ private:
 
   VkBuffer vertexBuffer;
   VkBuffer indexBuffer;
+
   VkDeviceMemory vertexBufferMemory;
   VkDeviceMemory indexBufferMemory;
 
@@ -175,7 +184,7 @@ private:
 #ifdef DEBUG
   const bool enableValidationLayers = true;
 #else
-  const bool enableValidationLayers = true;
+  const bool enableValidationLayers = false;
 #endif
 
   static void framebufferResizeCallback(GLFWwindow *window, int width, int height) {
@@ -1119,7 +1128,7 @@ private:
 
   void createTextureImage() {
     int texWidth, texHeight, texChannels;
-    stbi_uc *pixels        = stbi_load("textures/texture.png", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+    stbi_uc *pixels        = stbi_load(TEXTURE_PATH.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
     VkDeviceSize imageSize = texWidth * texHeight * 4;
 
     if (!pixels) { throw std::runtime_error("failed to load texture image!"); }
@@ -1189,6 +1198,42 @@ private:
 
     if (vkCreateSampler(device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
       throw std::runtime_error("failed to create texture sampler!");
+    }
+  }
+
+  void loadModel() {
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str())) {
+      throw std::runtime_error(warn + err);
+    }
+
+    std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
+    for (const auto &shape : shapes) {
+      for (const auto &index : shape.mesh.indices) {
+        Vertex vertex{};
+
+        vertex.pos = { attrib.vertices[3 * index.vertex_index + 0],
+          attrib.vertices[3 * index.vertex_index + 1],
+          attrib.vertices[3 * index.vertex_index + 2] };
+
+        vertex.texCoord = { attrib.texcoords[2 * index.texcoord_index + 0],
+          1.0f - attrib.texcoords[2 * index.texcoord_index + 1] };
+
+        vertex.color = { 1.0f, 1.0f, 1.0f };
+
+        if (uniqueVertices.count(vertex) == 0) {
+          uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+          vertices.push_back(vertex);
+        }
+
+        vertices.push_back(vertex);
+        indices.push_back(uniqueVertices[vertex]);
+      }
     }
   }
 
@@ -1397,7 +1442,7 @@ private:
     VkDeviceSize offsets[]   = { 0 };
 
     vkCmdBindVertexBuffers(commandBuffer, 0, vertexBuffers.size(), vertexBuffers.data(), offsets);
-    vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+    vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
     vkCmdBindDescriptorSets(
       commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSets[currentFrame], 0, nullptr);
 
@@ -1477,6 +1522,7 @@ private:
     createTextureImage();
     createTextureImageView();
     createTextureSampler();
+    loadModel();
     createVertexBuffer();
     createIndexBuffer();
     createUniformBuffer();
